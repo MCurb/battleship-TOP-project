ff319c3e6828d9bd7eb3dbdf280870571ce9a541
MCurb <marcoscurbeco03@gmail.com>
2025-11-25 10:18:08 -0600

    feat: add tests for isSunk method in Ship class

--- src/ship/ship-class.test.js
+++ src/ship/ship-class.test.js
@@ -1,3 +1,4 @@
+import { experiments } from 'webpack';
 import { Ship } from './ship-class';
 
 let ship;
@@ -8,4 +9,17 @@ beforeEach(() => {
 test('hit method', () => {
   ship.hit();
   expect(ship.hitCount).toBe(1);
+});
+
+describe('isSunk method', () => {
+  test('no hits yet', () => {
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test('being hitted', () => {
+    for (let i = 0; i <= ship.length; i++) {
+      ship.hit();
+    }
+    expect(ship.isSunk()).toBe(true);
+  });
 });
