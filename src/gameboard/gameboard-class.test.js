540af9125fe1b3b7d24f60033f8ad82eef438cf3
MCurb <marcoscurbeco03@gmail.com>
2025-11-25 16:45:10 -0600

    feat: add placeShip method tests for Gameboard class

--- src/gameboard/gameboard-class.test.js
+++ src/gameboard/gameboard-class.test.js
@@ -1,4 +1,3 @@
-
 import { Gameboard } from './gameboard-class';
 
 let gameBoard;
@@ -15,4 +14,23 @@ test('board creation', () => {
 
   expect(boardRows).toBe(gridSize);
   expect(boardColumns).toBe(gridSize);
+});
+
+describe('ship placement', () => {
+  beforeEach(() => {
+    gameBoard.createBoard(10);
+  });
+  test('place one ship', () => {
+    gameBoard.placeShip([5, 7], [5, 9], 2);
+    expect(typeof gameBoard.board[5][8]).toBe('object');
+  });
+
+  test('place multiple ships: horizontal and vertical', () => {
+    gameBoard.placeShip([5, 7], [5, 9], 2);
+    gameBoard.placeShip([5, 5], [8, 5], 3);
+    expect(typeof gameBoard.board[5][8]).toBe('object');
+    expect(typeof gameBoard.board[7][5]).toBe('object');
+  });
+
+  //Test if placeShip doesn't add a ship over another ship
 });
