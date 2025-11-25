9f854583d948eeb7cf5c3b7397a7a2588208ddc9
MCurb <marcoscurbeco03@gmail.com>
2025-11-25 16:46:09 -0600

    feat: add placeShip method to Gameboard class

--- src/gameboard/gameboard-class.js
+++ src/gameboard/gameboard-class.js
@@ -1,3 +1,5 @@
+import { Ship } from '../ship/ship-class';
+
 export class Gameboard {
   constructor() {
     this.board = [];
@@ -5,12 +7,24 @@ export class Gameboard {
 
   createBoard(gridSize) {
     for (let i = 0; i < gridSize; i++) {
-      const row = [];
+      const row = new Array(gridSize);
       this.board.push(row);
-      for (let i = 0; i < gridSize; i++) {
-        const cell = 0;
-        row.push(cell);
-      }
     }
+  }
+
+  placeShip(start, end, length) {
+    const ship = new Ship(length);
+
+    let [xs, ys] = start;
+    const [xe, ye] = end;
+    while (xs !== xe || ys !== ye) {
+      this.board[xs][ys] = ship;
+      if (xs < xe) xs++;
+      if (xs > xe) xs--;
+      if (ys < ye) ys++;
+      if (ys > ye) ys--;
+    }
+    //I need to check if a ship is already there, to avoid adding one ship
+    //over the other
   }
 }
