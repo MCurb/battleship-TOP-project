21327055aeb010670ebf66408445e6987daf1f39
MCurb <marcoscurbeco03@gmail.com>
2025-11-27 17:00:18 -0600

    feat: add a random ship button with functionality

--- src/game-ui.js
+++ src/game-ui.js
@@ -11,24 +11,29 @@ playerTwo.gameboard.createBoard(10);
 const playerOneBoard = document.querySelector('.player-one-board');
 const playerTwoBoard = document.querySelector('.player-two-board');
 
-export function createBoard(gridSize, playerBoard) {
+export function createBoard(gridSize, playerBoard, player) {
+  playerBoard.innerHTML = '';
   //Create rows:
   for (let i = 0; i < gridSize; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     playerBoard.appendChild(row);
-
     //Add cells:
     for (let j = 0; j < gridSize; j++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.dataset.cordinates = `${i}${j}`;
       row.appendChild(cell);
+      if (typeof player.gameboard.board[i][j] === 'object') {
+        cell.classList.add('ship');
+      } else if (player.gameboard.board[i][j] === 'attacked') {
+        cell.classList.add('attacked-cell');
+      }
     }
   }
 }
-createBoard(10, playerOneBoard);
-createBoard(10, playerTwoBoard);
+createBoard(10, playerOneBoard, playerOne);
+createBoard(10, playerTwoBoard, playerTwo);
 
 playerOneBoard.addEventListener('click', (e) => {
   const cell = e.target;
@@ -49,3 +54,14 @@ playerTwoBoard.addEventListener('click', (e) => {
     cell.classList.add('attacked-cell');
   }
 });
+
+//Place Ships:
+
+const randomShipPlayerOne = document.querySelector('.random-ships.player-one');
+randomShipPlayerOne.addEventListener('click', () => {
+  playerOne.gameboard.placeShip([4, 3], [4, 7], 2);
+  randomShipPlayerOne.style.background = 'blue';
+  createBoard(10, playerOneBoard, playerOne);
+});
+
+//Add more ships, and start working on attacking ships
